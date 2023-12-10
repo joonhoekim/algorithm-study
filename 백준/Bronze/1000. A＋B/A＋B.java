10e87455b3a29d145fe63aeebc7333497574d7ba
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-10 19:34:48 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int a =scanner.nextInt();
+        int b = scanner.nextInt();
+        java.lang.System.out.println(a+b);
+
+    }
+}
