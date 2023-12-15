d1aaba92ca0e709ba831a8cfdc5fa11292fda4e3
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-15 20:59:57 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    InputStream in = System.in;
+    Scanner sc = new Scanner(in);
+    int a = sc.nextInt();
+    int b= sc.nextInt();
+    System.out.print(a-b);
+  }
+}
