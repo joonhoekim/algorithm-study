8762473a8f163843a19cdbd32ac3e49f2512deb6
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-17 17:00:36 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner keyIn = new Scanner(System.in);
+    String id = keyIn.nextLine();
+    System.out.println(id + "??!");
+
+  }
+}
