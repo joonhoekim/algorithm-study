6570f39968de099227199bcc151fbb388e433ed6
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-17 17:27:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 200 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner keyIn = new Scanner(System.in);
+    long a = keyIn.nextLong();
+    long b = keyIn.nextLong();
+    long c = keyIn.nextLong();
+
+    System.out.print(a + b + c);
+
+  }
+}
