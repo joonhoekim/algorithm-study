fa6e1919c0658362bf81e310a8154d7f22f3eae3
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-22 19:34:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 888 ms, Memory: 250036 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int numberOfCase = Integer.parseInt(bufferedReader.readLine());
+    //System.out.println(numberOfCase);
+    StringBuilder stringBuilder = new StringBuilder();
+    StringTokenizer stringTokenizer;
+
+    for (int i = 0; i < numberOfCase; i++) {
+      stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+      stringBuilder.append(Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(
+          stringTokenizer.nextToken()) + "\n");
+    }
+
+    System.out.print(stringBuilder);
+
+  }
+}
