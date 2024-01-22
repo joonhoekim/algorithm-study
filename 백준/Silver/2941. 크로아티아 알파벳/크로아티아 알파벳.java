09d20c609e8c2292d9db1c505b2725378ae2d861
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-22 21:04:02 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder();
+    String[] croatians2 = {"c=", "c-", "d-", "lj", "nj", "s=", "z="};
+    String croatian3 = "dz=";
+    int toMinus = 0;
+
+    sb.append(sc.nextLine());
+
+    for (int i = 0; i < sb.length() - 2; i++) {
+      if (sb.substring(i, i + 3).equals(croatian3)) {
+        toMinus += 2;
+        sb.replace(i, i + 3, "xxx");
+      }
+    }
+
+    for (int i = 0; i < sb.length() - 1; i++) {
+      for (int j = 0; j < croatians2.length; j++) {
+        if (sb.substring(i, i + 2).equals(croatians2[j])) {
+          toMinus += 1;
+        }
+      }
+    }
+
+    System.out.print(sb.length() - toMinus);
+
+  }
+}
