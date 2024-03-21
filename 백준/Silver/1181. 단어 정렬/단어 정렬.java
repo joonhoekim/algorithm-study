9ccf091fcace40171e7f579722d7a10e08b3fc1c
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-22 00:49:28 +0900

    [Silver V] Title: 단어 정렬, Time: 384 ms, Memory: 23232 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,37 +1,44 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        // 입력 받기
+        int N = Integer.parseInt(br.readLine());
+        String[] words = new String[N];
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine().trim();
+        }
 
-    int N = Integer.parseInt(scanner.nextLine());
-    String[] words = new String[N];
-    for (int i = 0; i < N; i++) {
-      words[i] = scanner.nextLine().trim();
+        // 길이에 따라 정렬
+        Arrays.sort(words, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b); // 길이가 같으면 사전 순으로
+            } else {
+                return Integer.compare(a.length(), b.length()); // 길이가 짧은 것부터
+            }
+        });
+
+        // 중복 제거
+        ArrayList<String> uniqueWords = new ArrayList<>();
+        String prevWord = "";
+        for (String word : words) {
+            if (!word.equals(prevWord)) {
+                uniqueWords.add(word);
+                prevWord = word;
+            }
+        }
+
+        // 결과 출력
+        for (String word : uniqueWords) {
+            bw.write(word + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
-
-    Arrays.sort(words, (a, b) -> {
-      if (a.length() == b.length()) {
-        return a.compareTo(b); // 길이가 같으면 사전 순으로
-      } else {
-        return Integer.compare(a.length(), b.length()); // 길이가 짧은 것부터
-      }
-    });
-
-    // 중복 제거
-    ArrayList<String> uniqueWords = new ArrayList<>();
-    String prevWord = "";
-    for (String word : words) {
-      if (!word.equals(prevWord)) {
-        uniqueWords.add(word);
-        prevWord = word;
-      }
-    }
-
-    // 결과 출력
-    for (String word : uniqueWords) {
-      System.out.println(word);
-    }
-  }
 }
