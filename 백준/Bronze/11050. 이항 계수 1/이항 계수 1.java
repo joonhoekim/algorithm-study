3efa35bb4b5f84a52a084e7e2701a146344ee590
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-07 10:39:12 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 //이항계수 = combination
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int r = sc.nextInt();
-        System.out.println(combination(n, r));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(combination(n, r)));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     public static int combination(int n, int r) {
