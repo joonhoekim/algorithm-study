3401c6a631a8e7c4967fac5ec0163a9f03fe5225
joonhoekim <26rote@gmail.com>
2024-07-28 08:23:31 +0000

    C, D를 한번에 구하는 방법은 없어보임

--- codetree/exhaustiveSearch/ABCD찾기/Main.java
+++ codetree/exhaustiveSearch/ABCD찾기/Main.java
@@ -20,18 +20,21 @@ public class Main {
 
     Arrays.sort(arr);
 
-    // A+B+C+D - B+C+D = A
-    int a = arr[arr.length - 1] - arr[arr.length - 2];
+    // A와 B는 가장 작은 것이 확실함.
+    int a = arr[0];
+    int b = arr[1];
 
-    // A+B+C+D - A+C+D = B
-    int b = arr[arr.length - 1] - arr[arr.length - 3];
+    // arr[2]는 A+B[1,1,1,1]일수도 있고, C[1,1,9,9]일수도 있음.
+    // arr[3]또한 D라고 확신할 수 없음. A+B <= D 일 수도 있음.
 
-    // A+B+C+D - A+B+D = C
-    int c = arr[arr.length - 1] - arr[arr.length - 4];
+    //뒤에서 첫번째가 A+B+C+D인 것은 증명가능함
+    //뒤에서 두번째가 B+C+D인 것 또한 증명가능함
+    //뒤에서 세번째는 C+D[1,1,9,9] 이거나 A+B+C[9,9,9,9] 일 수도 있음.
 
-    // A+B+C+D - A+B+C = D
-    int d = arr[arr.length - 1] - arr[arr.length - 5];
-
+    //C 또는 D만 구하면 이 문제는 해결됨...
+    //A+B+C+D - [A+B] = C+D
+    //C+D를 구할 수 있음이 증명됨
+    // ... C와 D 각각을 한 번의 계산으로 구하는 방법은 없는 것 같음.
     System.out.printf("%d %d %d %d", a, b, c, d);
   }
 }
