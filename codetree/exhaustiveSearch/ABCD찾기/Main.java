9ed28dbfcbf9917eb0313ebde851f09903c73dc5
joonhoekim <26rote@gmail.com>
2024-07-28 08:37:28 +0000

    모든 가지수에 대해서 만족하는지 확인해보는 방식으로 풀어냄

--- codetree/exhaustiveSearch/ABCD찾기/Main.java
+++ codetree/exhaustiveSearch/ABCD찾기/Main.java
@@ -21,9 +21,6 @@ public class Main {
     Arrays.sort(arr);
 
     // A와 B는 가장 작은 것이 확실함.
-    int a = arr[0];
-    int b = arr[1];
-
     // arr[2]는 A+B[1,1,1,1]일수도 있고, C[1,1,9,9]일수도 있음.
     // arr[3]또한 D라고 확신할 수 없음. A+B <= D 일 수도 있음.
 
@@ -35,6 +32,38 @@ public class Main {
     //A+B+C+D - [A+B] = C+D
     //C+D를 구할 수 있음이 증명됨
     // ... C와 D 각각을 한 번의 계산으로 구하는 방법은 없는 것 같음.
-    System.out.printf("%d %d %d %d", a, b, c, d);
+    // 그래서 비효율적이어도 a,b,c,d를 임의로 정한 다음에, 정렬한 결과가 arr[]과 같은지 확인할 것임...
+
+    final int MAX_VALUE = 40;
+    loop:
+    for (int a = 1; a <= MAX_VALUE; a++) {
+      for (int b = 1; b <= MAX_VALUE; b++) {
+        for (int c = 1; c <= MAX_VALUE; c++) {
+          for (int d = 1; d <= MAX_VALUE; d++) {
+
+            boolean isSame = true;
+            int[] test = new int[]{
+                    a, b, c, d, a + b, b + c, c + d, d + a, a + c, b + d, a + b + c, a + c + d, a + b + d, b + c + d, a + b + c + d
+            };
+
+            Arrays.sort(test);
+            for (int i = 0; i < test.length; i++) {
+              if (test[i] != arr[i]) {
+                isSame = false;
+                break;
+              }
+            }
+            if (isSame) {
+              bw.write(String.format("%d %d %d %d", a, b, c, d));
+              break loop;
+            }
+
+          }
+        }
+      }
+    }
+    bw.flush();
+    br.close();
+    bw.close();
   }
 }
