54038e0fad88a0d47ae09c748e72a9407049f839
JoonhoeKim <26rote@gmail.com>
2024-08-12 16:23:33 +0900

    템플릿 추가 및 마지막 실력체크 문제 풀이

---
+++ codetree/template.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class template {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = Integer.MAX_VALUE;
+        //        int ans = Integer.MIN_VALUE;
+
+        //        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
