8a7e297078da6ec7f2236279c5eaca9d331b5903
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-07 14:41:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.64 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 93.8 MB, 시간: 75.97 ms
+메모리: 102 MB, 시간: 83.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 14:34:55
+2024년 09월 07일 14:41:22
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,30 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
         Map<String, Integer> map = new HashMap<>();
-        
-        // 후보자 기준으로 일단 다 세준다.
-        for(String candidate : participant) {
-            map.put(candidate, map.getOrDefault(candidate, 0) + 1);
-        }
-        
-        // 완주자 기준으로 하나씩 빼 준다.
-        for(String completed : completion) {
-            map.put(completed, map.getOrDefault(completed,0) - 1);
-        }
-        
-        // 이터레이터로 0이 아닌 candidate 이름을 찾는다.
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while(iterator.hasNext()) {
-            Map.Entry<String, Integer> entry = iterator.next();
-            if(entry.getValue() != 0) {
-                answer = entry.getKey();
-                break;
-            }
-        }
-        
-        return answer;
+
+        for (String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String c : completion) map.put(c, map.get(c) - 1);
+
+        return map.entrySet().stream()
+                .filter(e -> e.getValue() > 0)
+                .findFirst()
+                .get()
+                .getKey();
     }
 }
