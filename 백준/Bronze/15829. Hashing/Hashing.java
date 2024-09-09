72f4b1beea5f5bac87f967cf485a04bcb8eb20f7
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-09 16:56:34 +0900

    [Bronze II] Title: Hashing, Time: 168 ms, Memory: 17736 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,15 +9,17 @@ public class Main {
 
     int r = 31;
     int M = 1234567891;
-    //answer = hash value
-    int answer = 0;
+    long answer = 0;
+    long rPow = 1;
+
+    // 모듈러 연산의 분배 법칙을 아는지 확인하는 문제 
+    // (a * b) % M = ((a % M) * (b % M)) % M
     for (int i = 0; i < str.length(); i++) {
-      //a=1 ... z=26
-      int ch = str.charAt(i)-'a'+1;
-      answer += (int) ch * Math.pow(r, i);
+      int ch = str.charAt(i) - 'a' + 1;
+      answer = (answer + ch * rPow) % M;
+      rPow = (rPow * r) % M;
     }
 
     System.out.println(answer);
-
   }
 }
