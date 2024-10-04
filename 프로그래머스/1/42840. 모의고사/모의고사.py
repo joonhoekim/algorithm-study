cb50cbfb83ee0f8daa4c57f615e87f957a39c2fa
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-04 17:47:56 +0900

    [level 1] Title: 모의고사, Time: 9.36 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,4 +1,7 @@
 def solution(answers):
+    
+    result = []
+    
     patterns = [
         [1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5],
@@ -9,8 +12,11 @@ def solution(answers):
     
     for i, answer in enumerate(answers):
         for j, pattern in enumerate(patterns):
-            if answer == pattern[i % len(pattern)]:
-                scores[j] += 1
+            if answer == pattern[i%len(pattern)]:
+                scores[j]+=1
     
-    max_score = max(scores)
-    return [i + 1 for i, score in enumerate(scores) if score == max_score]
+    for i, score in enumerate(scores):
+        if score == max(scores):
+            result.append(i+1)
+    
+    return result
