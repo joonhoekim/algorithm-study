5d08c7868634147f195dda08577d6dd0f05b89ee
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-22 23:38:57 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -6,15 +6,6 @@ def solution(letter):
     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
     '-.--':'y','--..':'z'
     }
-    cursor_start = 0
-    cursor_end = 0
-    translated = []
-    list_letter = letter.split()
-    for i in range(len(list_letter)+1):
-        target = ''.join(list_letter[cursor_start:cursor_end])
-        if target in morse.keys():
-            cursor_start = cursor_end
-            translated.append(morse[target])
-        cursor_end += 1
-    return ''.join(translated)
+    
+    return ''.join(morse[x] for x in letter.split())
         
