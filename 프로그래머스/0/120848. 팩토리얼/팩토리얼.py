9e300516564b5c799605658760bf16315ed9d551
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-24 23:13:25 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,12 +1,17 @@
 import math
 
 def solution(n):
-    # 1 부터 x 까지 하나씩 늘려보면서 처리하는 건 너무 비효율적인 것 같고...
-    # factorial의 대략적인 값을 유추할 수 있나?
-    max = 1
-    for i in range(2, n+1):
-        if math.factorial(i) > n:
+    answer = 0
+    acc = 1
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    for i in range(2, 12):
+        acc *= i
+        if acc > n:
+            answer = i-1
             break
-        else:
-            max = i
-    return max
+    return answer
