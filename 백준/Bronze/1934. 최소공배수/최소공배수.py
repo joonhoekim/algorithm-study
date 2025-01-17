bed902236556eaea6b9b7ebcdcbada0cdbd7c494
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2025-01-17 23:57:10 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    print(lcm(A, B))
